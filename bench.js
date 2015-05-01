c01cb044e1f7190a8fabf1a6719e86afabb575b7
Jordan Rousseau <jrousseau@wdtinc.com>
2015-05-01 11:17:47 -0500

    updated tests/benchmarks for both rtree indexed and non-indexed fc

--- bench.js
+++ bench.js
@@ -1,19 +1,25 @@
 var nearest = require('./');
+var index = require('turf-index');
 var Benchmark = require('benchmark');
 var fs = require('fs');
 
 var pt = JSON.parse(fs.readFileSync(__dirname+'/geojson/pt.geojson'));
 var pts = JSON.parse(fs.readFileSync(__dirname+'/geojson/pts.geojson'));
+var index_pts = index(pts, 'rtree');
 
 var suite = new Benchmark.Suite('turf-nearest');
+
 suite
-  .add('turf-nearest',function () {
+  .add('turf-nearest (non-indexed)',function () {
     nearest(pt, pts);
+  })
+  .add('turf-nearest (indexed)',function () {
+      nearest(pt, index_pts);
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
   })
   .on('complete', function () {
-    
+
   })
   .run();
--- test.js
+++ test.js
@@ -1,16 +1,25 @@
 var test = require('tape');
 var nearest = require('./');
+var index = require('turf-index');
 var fs = require('fs');
 
 test('distance', function(t){
   var pt = JSON.parse(fs.readFileSync(__dirname+'/geojson/pt.geojson'));
   var pts = JSON.parse(fs.readFileSync(__dirname+'/geojson/pts.geojson'));
+  var indexed_pts = index(pts, 'rtree');
 
   var closestPt = nearest(pt, pts);
+  var indexedPt = nearest(pt, indexed_pts);
 
-  t.ok(closestPt, 'should return a point');
+  t.ok(closestPt, 'should return a point (non-indexed)');
   t.equal(closestPt.geometry.type, 'Point', 'should be a point');
   t.equal(closestPt.geometry.coordinates[0], -75.33, 'lon -75.33');
   t.equal(closestPt.geometry.coordinates[1], 39.44, 'lat 39.44');
+
+
+  t.ok(indexedPt, 'should return a point (indexed)');
+  t.equal(indexedPt.geometry.type, 'Point', 'should be a point');
+  t.equal(indexedPt.geometry.coordinates[0], -75.33, 'lon -75.33');
+  t.equal(indexedPt.geometry.coordinates[1], 39.44, 'lat 39.44');
   t.end();
 });
